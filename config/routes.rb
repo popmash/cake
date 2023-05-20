07821fad89faa9d2c3f018e5142857e1d88561bc
popmash <131745061+popmash@users.noreply.github.com>
2023-05-20 07:49:30 +0000

    Up

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  
+  root to: 'homes#top'
+  get 'homes/about' => 'homes#about',as:'about'
+  
+  devise_for :customers, controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  } 
+   devise_for :admin,skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  
+   resources :customers, only:[:info]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
